aa7a1abb085661f87990aa0477d74879f9c473fd
Dofufa Labs <121826629+dofufa@users.noreply.github.com>
2023-08-01 17:42:45 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-/* */
+$( document ).ready(function(){
+  
+  $( 'button' ).click( function(){
+    console.log('foo!');
+    /*  
+    gtag('event', 'button-click', {
+      'app_name': 'dofufa-feedback',
+      'app_touch': 'feedback-home-button'
+    });
+    */
+  });
+
+}
+                    
